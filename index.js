be869830d63557293658e3d83c64171b4b643e29
arunsridher <45627741+arunsridher@users.noreply.github.com>
2021-02-20 15:51:44 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const prettyjson = require("prettyjson");
+const express = require("express");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+
+const options = {
+  noColor: true,
+};
+
+// create an express app and configure it with boadyParser middleware
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
+
+// create our webhook endpoint to recive webhooks from Safaricom
+app.post("/hooks/mpesa", (req, res) => {
+  filePath = __dirname + "/public/data.txt";
+  const data = JSON.stringify(req.body);
+  fs.appendFile(filePath, data, function () {
+    console.log("DONE");
+    res.end();
+  });
+});
+
+app.get("/", (req, res) => {
+  let message = {
+    ResponseCode: "00000000",
+    ResponseDesc: "success",
+  };
+
+  // respond to safaricom servers with a success message
+  return res.json(message);
+});
+
+const server = app.listen(5000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log(`server listening on port ${port}`);
+});
